2431b2259f921bd6a5dc0045a40d32f8d6eabf87
Patrícia Paulo <44577628+Qetsyah@users.noreply.github.com>
2022-11-22 15:38:57 +0000

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -5,7 +5,7 @@ const { ethers } = require("hardhat");
 
 async function main() {
 //address that will receive the funds
-  const address = "0x17639dd8fb689223CB19a80c6a9182211C8Df0d5"; //Second account in personal Patricia's metamask
+  const address = "0x17639dd8fb689223CB19a80c6a9182211C8Df0d5";
   const getFunds = await helpers.setBalance(
     address,
     ethers.utils.parseEther('100000')
